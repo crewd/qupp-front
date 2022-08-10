6d709238adf56e9ab871d9570ab27c40318255f1
crewd <newspaper321@nate.com>
2022-08-11 05:40:15 +0900

    chore: tailind css font size custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        regular: "16px",
+        max: "40px",
+        xxl: "34px",
+        xl: "26px",
+        lg: "24px",
+        md: "20px",
+        sm: "12px",
+        xs: "10px",
+      },
+    },
   },
   plugins: [],
 }
