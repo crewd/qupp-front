1fdbf46438fe279e4669cc8f5587da760c4bb144
crewd <newspaper321@nate.com>
2022-08-12 04:28:22 +0900

    design: 메인페이지 style 추가

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -9,9 +10,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           <button className="w-[90px] h-[40px] bg-primary text-white rounded-3xl">
             로그인
           </button>
-          <button className="w-[90px] h-[40px] ml-[30px] rounded-3xl">
-            회원가입
-          </button>
+          <Link href="/">
+            <a className="w-[90px] h-[40px] ml-[50px] rounded-3xl">회원가입</a>
+          </Link>
         </nav>
       </header>
       <main className="my-[20px]">{children}</main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Home: NextPage = () => {
   return (
@@ -12,21 +15,55 @@ const Home: NextPage = () => {
         />
       </Head>
       <div className="h-[100vh] bg-gray-5 fixed w-full top-0 left-0 right-0 -z-10"></div>
-      <div className="mt-[56px]">
-        <div className="mt-[160px] font-bold">
-          <p className="text-md">인문부터 예술까지 어느 분야든</p>
-          <br />
-          <p className="text-xxl">직접 해결하기 어려운</p>
-          <p className="text-xxl">대학교 공부는</p>
-          <p className="text-xxl">커프에서 물어보세요!</p>
-        </div>
-        <div>
-          <div className="bg-white rounded-xl p-[15px] w-[99px] h-[124px]">
-            <p className="font-bold text-gray-3">
-              내가 <br /> 등록한
-              <br /> <span className="text-secondary-sBlue">질문</span>
-            </p>
+      <div className="mt-[56px] w-[1100px] flex justify-between">
+        <div className="mt-[95px] font-bold">
+          <div className="mb-[50px]">
+            <p className="text-md mb-[10px]">인문부터 예술까지 어느 분야든</p>
+            <p className="text-max leading-[80px]">직접 해결하기 어려운</p>
+            <p className="text-max leading-[80px]">대학교 공부는</p>
+            <p className="text-max leading-[80px]">커프에서 물어보세요!</p>
           </div>
+          <button className="rounded-3xl bg-primary w-[138px] h-[52px] text-white">
+            바로 질문하기
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-[100px]">
+          <Link href="/">
+            <a className="bg-white rounded-3xl p-[15px] w-[99px] h-[124px]">
+              <p className="font-bold text-gray-3">
+                내가 <br /> 등록한
+                <br /> <span className="text-secondary-sBlue">질문</span>
+              </p>
+              <FontAwesomeIcon
+                className="text-secondary-sBlue"
+                icon={faArrowRight}
+              />
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="bg-white rounded-3xl p-[15px] w-[99px] h-[124px]">
+              <p className="font-bold text-gray-3">
+                내가 <br /> 등록한
+                <br /> <span className="text-secondary-sBlue">답변</span>
+              </p>
+              <FontAwesomeIcon
+                className="text-secondary-sBlue"
+                icon={faArrowRight}
+              />
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="bg-white rounded-3xl p-[15px] w-[99px] h-[124px]">
+              <p className="font-bold text-secondary-sBlue pt-[15px] leading-[20px]">
+                즐겨찾기
+                <br /> <span className="text-gray-3">목록</span>
+              </p>
+              <FontAwesomeIcon
+                className="text-secondary-sBlue mt-[23px]"
+                icon={faArrowRight}
+              />
+            </a>
+          </Link>
         </div>
       </div>
     </div>
