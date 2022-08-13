58d27730f0297a9b99abd4bb977b4fed83fe2a2c
crewd <newspaper321@nate.com>
2022-08-14 04:24:47 +0900

    feat: editor 추가

---
+++ components/editor.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const QuillEditor = () => {
+  const toolbarOptions = [
+    [{ header: [1, 2, 3, false] }],
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ color: [] }, { background: [] }],
+    [{ align: [] }],
+    ['link', 'image'],
+  ];
+  const formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'align',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'background',
+    'color',
+    'link',
+    'image',
+    'video',
+    'width',
+  ];
+
+  const modules = {
+    toolbar: {
+      container: toolbarOptions,
+    },
+  };
+
+  return (
+    <ReactQuill
+      className="h-[500px] border-pimary"
+      theme="snow"
+      modules={modules}
+      formats={formats}
+      style={{ borderColor: '#6096FD' }}
+    />
+  );
+};
+
+export default QuillEditor;
--- pages/qna/write.tsx
+++ pages/qna/write.tsx
@@ -1,6 +1,11 @@
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
+
+const WysiwygEditor = dynamic(() => import('../../components/editor'), {
+  ssr: false,
+});
 
 const AddPost = () => {
   const category = ['인문', '사회', '상경', '자연과학', '공학', '예술'];
@@ -21,14 +26,14 @@ const AddPost = () => {
       <div className="mb-[20px]">
         <h2 className="text-xl font-bold text-primary">질문 등록하기</h2>
       </div>
-      <div className="border-2 shadow-xl rounded-lg p-[10px]">
+      <div className="border-2 shadow-xl rounded-lg p-[20px] flex flex-col gap-[10px]">
         <div className="flex text-center">
-          <p className="text-white bg-primary w-[130px] rounded-tl-lg rounded-bl-lg p-[10px]">
+          <p className="text-white bg-primary h-[45px] w-[130px] leading-[25px] rounded-tl-lg rounded-bl-lg p-[10px]">
             계열
           </p>
           <div className="relative">
             <div
-              className={`p-[10px] w-[130px] border border-primary rounded-tr-lg ${
+              className={`p-[10px] w-[130px] h-[45px] border border-primary rounded-tr-lg ${
                 !selectOpened && 'rounded-br-lg'
               }`}
             >
@@ -36,12 +41,12 @@ const AddPost = () => {
                 className="flex items-center justify-between w-full"
                 onClick={() => setSelectOpened(!selectOpened)}
               >
-                <span className="w-full">{selectTopic}</span>
+                <span className="w-full leading-[21px]">{selectTopic}</span>
                 <FontAwesomeIcon className="" icon={faChevronDown} />
               </button>
             </div>
             {selectOpened && (
-              <ul className="absolute top-[100%] bg-white w-[130px] border border-primary border-t-0 rounded-b-lg">
+              <ul className="absolute top-[100%] bg-white w-[130px] border border-primary border-t-0 rounded-b-lg shadow-lg">
                 {category.map((data) => (
                   <li
                     className="py-[5px] hover:bg-gray-100 cursor-pointer"
@@ -52,6 +57,23 @@ const AddPost = () => {
                 ))}
               </ul>
             )}
+          </div>
+        </div>
+        <div className="flex text-center">
+          <p className="text-white h-[45px] leading-[24px] bg-primary w-[130px] rounded-tl-lg rounded-bl-lg p-[10px]">
+            제목
+          </p>
+          <input
+            className="w-[calc(100%-130px)] border rounded-tr-lg rounded-br-lg outline-none border-primary px-[10px]"
+            type="text"
+          />
+        </div>
+        <div className="flex text-center">
+          <p className="text-white bg-primary w-[130px] h-[45px]  leading-[24px] rounded-tl-lg rounded-bl-lg p-[10px]">
+            내용
+          </p>
+          <div className="w-[calc(100%-130px)] h-[550px]">
+            <WysiwygEditor />
           </div>
         </div>
       </div>
