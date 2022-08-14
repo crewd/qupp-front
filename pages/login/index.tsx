f6e2bdd341b874fd69e1d9818d04cedd95ba1a12
crewd <newspaper321@nate.com>
2022-08-15 01:57:39 +0900

    design: 회원가입, 로그인페이지 레이아웃 변경

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -9,33 +9,38 @@ const LoginPage = () => {
         <meta name="description" content="로그인" />
       </Head>
       <BgTitle>로그인</BgTitle>
-      <div className="sm:w-[480px] w-full sm:px-[30px] sm:py-[40px] m-auto flex justify-center flex-col sm:border sm:shadow-xl sm:rounded-3xl">
-        <div className="w-full flex flex-col gap-[20px]">
-          <div className="w-full text-gray-1">
-            <p className="mb-[15px]">이메일</p>
+      <div className="sm:w-[620px] w-full sm:px-[30px] sm:py-[40px] m-auto flex justify-center flex-col sm:border sm:shadow-xl sm:rounded-3xl">
+        <div className="w-full flex flex-col gap-[20px] items-center">
+          <div className="flex justify-between w-full text-gray-1">
+            <p className="leading-[42px] font-bold">이메일</p>
             <input
-              className="outline-none border w-full border-primary rounded-md px-[10px] py-[8px]"
+              className="outline-none border w-[480px] border-primary rounded-md px-[10px] py-[8px]"
               type="email"
               placeholder="이메일"
             />
           </div>
-          <div className="w-full text-gray-1">
-            <p className="mb-[15px]">비밀번호</p>
+          <div className="flex justify-between w-full text-gray-1">
+            <p className="leading-[42px] font-bold">비밀번호</p>
             <input
-              className="outline-none border w-full border-primary rounded-md px-[10px] py-[8px]"
+              className="outline-none border w-[480px] border-primary rounded-md px-[10px] py-[8px]"
               type="password"
               placeholder="비밀번호"
             />
           </div>
-          <div className="mt-[30px] flex flex-col gap-[20px]">
-            <button className="w-full h-[40px] outline-none text-white  bg-primary rounded-md">
+          <div className="mt-[30px] flex gap-[20px] m-auto w-[355px]">
+            <button className="w-full h-[52px] outline-none font-bold text-white  bg-primary rounded-xl">
               로그인
             </button>
-            <button className="w-full h-[40px] outline-none text-primary border border-primary bg-white rounded-md">
+            <button className="w-full h-[52px] outline-none font-bold text-primary border border-primary bg-white rounded-xl">
               회원가입
             </button>
           </div>
         </div>
+      </div>
+      <div className="flex justify-center gap-[10px] pt-[20px] text-gray-4">
+        <p>아이디찾기</p>
+        <p>|</p>
+        <p>비밀번호 찾기</p>
       </div>
     </div>
   );
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -9,46 +9,43 @@ const SignUpPage = () => {
         <meta name="description" content="회원가입" />
       </Head>
       <BgTitle>회원가입</BgTitle>
-      <div className="sm:w-[600px] w-full sm:px-[60px] sm:py-[40px] m-auto flex justify-center flex-col sm:border sm:shadow-xl sm:rounded-3xl">
+      <div className="sm:w-[620px] w-full sm:px-[30px] sm:py-[40px] m-auto flex justify-center flex-col sm:border sm:shadow-xl sm:rounded-3xl">
         <div className="w-full flex flex-col gap-[20px]">
-          <div className="w-full text-gray-1">
-            <p className="mb-[15px]">이메일</p>
+          <div className="flex justify-between w-full text-gray-1">
+            <p className="leading-[42px] font-bold">이메일</p>
             <input
-              className="outline-none border w-full border-primary rounded-md px-[10px] py-[8px]"
+              className="outline-none border w-[440px] border-primary rounded-md px-[10px] py-[8px]"
               type="email"
               placeholder="이메일"
             />
           </div>
-          <div className="w-full text-gray-1">
-            <p className="mb-[15px]">비밀번호</p>
+          <div className="flex justify-between w-full text-gray-1">
+            <p className="font-bold laeding-[42px]">비밀번호</p>
             <input
-              className="outline-none border w-full border-primary rounded-md px-[10px] py-[8px]"
+              className="outline-none border w-[440px] border-primary rounded-md px-[10px] py-[8px]"
               type="password"
               placeholder="비밀번호"
             />
           </div>
-          <div className="w-full text-gray-1">
-            <p className="mb-[15px]">비밀번호 확인</p>
+          <div className="flex justify-between w-full text-gray-1">
+            <p className="leading-[42px] font-bold">비밀번호 확인</p>
             <input
-              className="outline-none border w-full border-primary rounded-md px-[10px] py-[8px]"
+              className="outline-none border w-[440px] border-primary rounded-md px-[10px] py-[8px]"
               type="password"
               placeholder="비밀번호 확인"
             />
           </div>
-          <div className="w-full text-gray-1">
-            <p className="mb-[15px]">닉네임</p>
+          <div className="flex justify-between w-full text-gray-1">
+            <p className="font-bold laeding-[42px]">닉네임</p>
             <input
-              className="outline-none border w-full border-primary rounded-md px-[10px] py-[8px]"
+              className="outline-none border w-[440px] border-primary rounded-md px-[10px] py-[8px]"
               type="text"
               placeholder="닉네임"
             />
           </div>
-          <div className="mt-[30px] flex flex-col gap-[20px]">
-            <button className="w-full h-[40px] outline-none text-white  bg-primary rounded-md">
+          <div className="mt-[30px] flex justify-center">
+            <button className="w-[165px] h-[52px] outline-none font-bold text-primary bg-white border border-primary rounded-md">
               가입하기
-            </button>
-            <button className="w-full h-[40px] outline-none text-primary border border-primary bg-white rounded-md">
-              돌아가기
             </button>
           </div>
         </div>
