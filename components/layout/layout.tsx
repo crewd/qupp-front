5b985f46fef16844cdaad1b9ffd6ede9d6671dd9
crewd <newspaper321@nate.com>
2022-08-24 03:34:30 +0900

    feat: 헤더 게시판, 글 작성 메뉴 추가

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -11,21 +11,35 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
               <h2 className="font-bold text-xxl">QUPP</h2>
             </a>
           </Link>
-          <nav className="flex justify-between">
-            <button className="lg:w-[90px] w-[60px] lg:h-[40px] h-[30px] bg-primary text-white rounded-3xl">
+          <nav className="flex justify-between w-full">
+            <div className="flex ml-[40px] font-bold">
               <Link href="/login">
-                <a className="block lg:h-[40px] h-[30px] lg:leading-[40px] leading-[30px]">
-                  로그인
+                <a className="lg:h-[40px] h-[30px] lg:leading-[40px] leading-[30px] hover:text-primary">
+                  게시판
                 </a>
               </Link>
-            </button>
-            <button className="lg:w-[90px] w-[60px] lg:h-[40px] h-[30px] ml-[20px] rounded-3xl">
-              <Link href="/signup">
-                <a className="block lg:h-[40px] h-[30px] lg:leading-[40px] leading-[30px]">
-                  회원가입
+              <Link href="/login">
+                <a className="ml-[40px] lg:h-[40px] h-[30px] lg:leading-[40px] leading-[30px] hover:text-primary">
+                  글 작성하기
                 </a>
               </Link>
-            </button>
+            </div>
+            <div className="font-bold">
+              <button className="lg:w-[90px] w-[60px] lg:h-[40px] h-[30px] bg-primary text-white rounded-3xl">
+                <Link href="/login">
+                  <a className="block lg:h-[40px] h-[30px] lg:leading-[40px] leading-[30px]">
+                    로그인
+                  </a>
+                </Link>
+              </button>
+              <button className="lg:w-[90px] w-[60px] lg:h-[40px] h-[30px] ml-[20px] rounded-3xl">
+                <Link href="/signup">
+                  <a className="block lg:h-[40px] h-[30px] lg:leading-[40px] leading-[30px]">
+                    회원가입
+                  </a>
+                </Link>
+              </button>
+            </div>
           </nav>
         </div>
       </header>
